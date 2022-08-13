f414712e3172b4042ddb571e498fde43bf7c1c62
grzechz <grzechz@protonmail.com>
2022-08-13 18:53:27 +0200

    feat: improve fonts load

--- src/components/Document.server.tsx
+++ src/components/Document.server.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react";
+import { Fonts } from "./Fonts.server";
 
 export const Document: FC<{
   children: ReactNode;
@@ -9,19 +10,11 @@ export const Document: FC<{
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>template-with-fastify</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet"
-      />
+      <Fonts.Preconnect />
     </head>
     <body>
       <div id="root">{children}</div>
+      <Fonts.Style />
     </body>
   </html>
 );
